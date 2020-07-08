de4bb06ffc6c60e0f82392d94de40493d8580d35
Clinton Moshe <clintonmwachia9@gmail.com>
2020-07-09 02:18:30 +0300

    Data Visualization

---
+++ Data Visualization.R
@@ -0,0 +1,66 @@
+options(device = "windows")
+# loading the library
+library(tidyverse)
+
+# the dataset to use
+mpg
+
+# creating a ggplot
+windows();
+ggplot(data = mpg) +
+  geom_point(mapping = aes(x = displ, y = hwy))
+
+# number of rows
+nrow(mtcars)
+ncol(mtcars)
+
+# hwy vs cyl
+ggplot(data = mpg) +
+  geom_point(mapping = aes(x = cyl, y = hwy))
+
+# class vs drv
+ggplot(data = mpg) +
+  geom_point(mapping = aes(x = class, y = drv))
+
+# displ vs hwy by class
+ggplot(data = mpg) +
+  geom_point(mapping = aes(x = displ, y = hwy, color = class, size = 3))
+
+ggplot(data = mpg) +
+  geom_point(mapping = aes(x = displ, y = hwy, size = class))
+
+ggplot(data = mpg) +
+  geom_point(mapping = aes(x = displ, y = hwy, alpha = class))
+
+ggplot(data = mpg) +
+  geom_point(mapping = aes(x = displ, y = hwy, shape = class))
+
+ggplot(data = mpg) +
+  geom_point(mapping = aes(x = displ, y = hwy), color = 'red')
+
+
+## Facets
+ggplot(data = mpg) +
+  geom_point(mapping = aes(x = displ, y = hwy)) +
+  facet_wrap(~ class, nrow = 3)
+
+ggplot(data = mpg) +
+  geom_point(mapping = aes(x = displ, y = hwy)) +
+  facet_wrap(drv ~ cyl)
+
+# facet with a continuos variable
+ggplot(data = mpg) +
+  geom_point(mapping = aes(x = displ, y = hwy)) +
+  facet_wrap(~ displ)
+
+# rrow wise plot
+ggplot(data = mpg) +
+  geom_point(mapping = aes(x = displ, y = hwy)) +
+  facet_grid(cyl ~ .)
+
+# column wise plot
+ggplot(data = mpg) +
+  geom_point(mapping = aes(x = displ, y = hwy)) +
+  facet_grid(. ~ drv)
+
+# GEOMETRIC POINTS
