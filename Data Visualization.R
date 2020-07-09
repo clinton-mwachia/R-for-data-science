1eb4f5f4213dcd774efee41abcf4c376983e37e1
Clinton Moshe <clintonmwachia9@gmail.com>
2020-07-09 20:15:07 +0300

    Data Viz

--- Data Visualization.R
+++ Data Visualization.R
@@ -1,4 +1,3 @@
-options(device = "windows")
 # loading the library
 library(tidyverse)
 
@@ -63,4 +62,36 @@ ggplot(data = mpg) +
   geom_point(mapping = aes(x = displ, y = hwy)) +
   facet_grid(. ~ drv)
 
-# GEOMETRIC POINTS
+# GEOMETRIC OBJECTS
+ggplot(data = mpg) + 
+  geom_smooth(mapping = aes(x = displ, y = hwy, linetype = drv))
+
+ggplot(data = mpg) + 
+  geom_smooth(mapping = aes(x = displ, y = hwy))
+
+ggplot(data = mpg) + 
+  geom_smooth(mapping = aes(x = displ, y = hwy, group = drv))
+
+ggplot(data = mpg) + 
+  geom_smooth(mapping = aes(x = displ, y = hwy, color = drv),
+              show.legend = FALSE)
+
+ggplot(data = mpg, mapping = aes(x = displ, y = hwy)) + 
+  geom_point() +
+  geom_smooth()
+
+ggplot(data = mpg, mapping = aes(x = displ, y = hwy)) + 
+  geom_point(mapping = aes(color = class)) +
+  geom_smooth()
+
+ggplot(data = mpg, mapping = aes(x = displ, y = hwy)) + 
+  geom_point(mapping = aes(color = class)) +
+  geom_smooth(
+    data = filter(mpg, class == "subcompact"), se = FALSE
+  )
+
+ggplot(data = mpg, mapping = aes(x = displ, y = hwy, color = drv)) +
+  geom_point(show.legend = TRUE) +
+  geom_smooth(se = TRUE)
+
+# STATISTICAL TRANSFORMATIONS
