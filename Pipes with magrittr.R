df6faa7edead594a30cadfade21ca7441806a506
Clinton Moshe <clintonmwachia9@gmail.com>
2020-08-28 00:08:28 +0300

    pipes with magrittr

---
+++ Pipes with magrittr.R
@@ -0,0 +1,28 @@
+# pipes are used for a sequence of operations
+library(magrittr)
+
+diamonds <- ggplot2::diamonds
+View(diamonds)
+diamonds2 <- diamonds %>%
+  dplyr::mutate(price_per_carat = price / carat)
+
+# object size
+# install.packages('pryr', dependencies = T)
+# amount of ram occuppied
+pryr::object_size(diamonds)
+pryr::object_size(diamonds2)
+diamonds$carat[1] <- NA
+diamonds
+pryr::object_size(diamonds)
+object.size(diamonds)
+
+windows()
+rnorm(100) %>%
+  matrix(ncol = 2) %>%
+  plot() %>%
+  str()
+
+rnorm(100) %>%
+  matrix(ncol = 2) %T>%
+  plot() %>%
+  str()
