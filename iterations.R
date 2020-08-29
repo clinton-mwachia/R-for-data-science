6d834feefd67495329e1a0f450228c2e7ebe5648
Clinton Moshe <clintonmwachia9@gmail.com>
2020-08-29 11:37:11 +0300

    iterations

---
+++ iterations.R
@@ -0,0 +1,27 @@
+library(tidyverse)
+
+# FOR LOOPS
+df <- tibble(
+  a = rnorm(10),
+  b = rnorm(10),
+  c = rnorm(10),
+  d = rnorm(10)
+)
+df
+
+# median
+seq_along(df)
+
+vec <- vector('double', ncol(df))
+for (i in seq_along(df)) {
+  vec[i] <- median(df[[i]])
+}
+vec
+sapply(df, median)
+
+# the map function
+map_dbl(df, median)
+map_dbl(df, mean)
+map_dbl(df, sd)
+
+map_dbl(df, length)
